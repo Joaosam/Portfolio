c7164c9f56bc82ec8475c9de12a64225c2346a32
joaosam <joao99.jp@gmail.com>
2022-09-19 19:19:39 -0300

    Clear toggleButton

--- js/toggleButton.js
+++ js/toggleButton.js
@@ -2,8 +2,7 @@ const btnMobile = document.getElementById('btnMobile')
 
 const toggleMenu = () => {
   const nav = document.querySelector('.navigation')
-  const toggleNav = nav.classList.toggle('active')
-  console.log(toggleNav)
+  nav.classList.toggle('active')
 
   const classTagA = [
     '.goToHome',
